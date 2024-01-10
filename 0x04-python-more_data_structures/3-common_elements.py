1248db33ddcceff8d9207443f69ef5038a883c30
ykelly <yk.kelly.kelly@gmail.com>
2024-01-10 10:06:58 -0800

    Soul

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
-def common_elements(set_1, set_2):
-    """
-    A function that returns a set of
-    common elements in two sets
-    """
-    return(set_1 & set_2)
+def print_reversed_list_integer(my_list=[]):
+    if isinstance(my_list, list):
+        my_list.reverse()
+        for i in my_list:
+            print("{:d}".format(i))
