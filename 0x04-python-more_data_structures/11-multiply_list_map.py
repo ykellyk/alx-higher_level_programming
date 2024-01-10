7ecd04ae5a582ea3ca53be09f98bbf8933991ae4
ykelly <yk.kelly.kelly@gmail.com>
2024-01-10 10:56:10 -0800

    Soul

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3
-
 def multiply_list_map(my_list=[], number=0):
-    newlisst = [map(lambda x: x * number, my_list)]
-    return (newlisst)
+    return list(map(lambda x: x * number, my_list))
+
