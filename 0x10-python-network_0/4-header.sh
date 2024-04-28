8536b5277726a32b46614dfaad70fb4b087cfef9
ykelly <yk.kelly.kelly@gmail.com>
2024-04-28 13:26:40 -0700

    Klit

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script that takes in a URL as an argument, sends a GET request to the URL
-curl -H "X-HolbertonSchool-User-Id:98" -sLX GET "$1"
+# Sends a GET request to a URL, adds a new request header and outputs the body of the response
+curl -s -X GET -H "X-School-User-Id: 98" "$1"
