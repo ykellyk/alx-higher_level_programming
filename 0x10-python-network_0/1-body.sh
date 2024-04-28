9ddd3c12923a17f3085e91d6cd5abb11d65921ed
ykelly <yk.kelly.kelly@gmail.com>
2024-04-28 13:17:01 -0700

    Klit

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request to a given URL and display the response status code.
-curl -s -o /dev/null -w "%{http_code}" "$1"
+#send a bash script to a URL and display the body of the response
+curl -sL --fail -X GET "$1";
