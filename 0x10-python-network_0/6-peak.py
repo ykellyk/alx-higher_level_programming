85032e30f214e9739201ad3c266dcffe95b308c9
ykelly <yk.kelly.kelly@gmail.com>
2024-04-28 13:30:28 -0700

    Klit

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-"""Finds a peak in a list of unsorted integers"""
-
 
 def find_peak(list_of_integers):
-    """Finds a peak in list_of_integers"""
-
-    if list_of_integers is None or list_of_integers == []:
+    """Finds peak
+    Args:
+        list_of_integers (list): list of integers
+    """
+    len_lst = len(list_of_integers)
+    if len_lst < 3:
         return None
-    lo = 0
-    hi = len(list_of_integers)
-    mid = ((hi - lo) // 2) + lo
-    mid = int(mid)
-    if hi == 1:
-        return list_of_integers[0]
-    if hi == 2:
-        return max(list_of_integers)
-    if list_of_integers[mid] >= list_of_integers[mid - 1] and\
-            list_of_integers[mid] >= list_of_integers[mid + 1]:
-        return list_of_integers[mid]
-    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
-        return find_peak(list_of_integers[mid:])
-    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
+    peak = list_of_integers[1]
+    for i in range(1, len_lst):
+        if list_of_integers[i] >= peak:
+            peak = list_of_integers[i]
+    return peak
+
+if __name__ == "__main__":
+    print(find_peak([1, 2, 4, 6, 3]))
+    print(find_peak([4, 2, 1, 2, 3, 1]))
+    print(find_peak([2, 2, 2]))
+    print(find_peak([]))
+    print(find_peak([-2, -4, 2, 1]))
+    print(find_peak([4, 2, 1, 2, 3, 1]))
