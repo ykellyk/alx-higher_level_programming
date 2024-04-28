5d04455268fede5bd9b00146269d1af0300fde7a
ykelly <yk.kelly.kelly@gmail.com>
2024-04-28 13:32:22 -0700

    Klit

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,23 +1,10 @@
 #!/usr/bin/python3
+""" Test function find_peak """
+find_peak = __import__('6-peak').find_peak
 
-def find_peak(list_of_integers):
-    """Finds peak
-    Args:
-        list_of_integers (list): list of integers
-    """
-    len_lst = len(list_of_integers)
-    if len_lst < 3:
-        return None
-    peak = list_of_integers[1]
-    for i in range(1, len_lst):
-        if list_of_integers[i] >= peak:
-            peak = list_of_integers[i]
-    return peak
-
-if __name__ == "__main__":
-    print(find_peak([1, 2, 4, 6, 3]))
-    print(find_peak([4, 2, 1, 2, 3, 1]))
-    print(find_peak([2, 2, 2]))
-    print(find_peak([]))
-    print(find_peak([-2, -4, 2, 1]))
-    print(find_peak([4, 2, 1, 2, 3, 1]))
+print(find_peak([1, 2, 4, 6, 3]))
+print(find_peak([4, 2, 1, 2, 3, 1]))
+print(find_peak([2, 2, 2]))
+print(find_peak([]))
+print(find_peak([-2, -4, 2, 1]))
+print(find_peak([4, 2, 1, 2, 2, 2, 3, 1]))
